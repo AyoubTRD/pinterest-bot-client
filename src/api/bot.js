5e19abf27f7ed323b934a5da9035741a4898e3c8
JrTRD <ayoubtrdbuzzer@gmail.com>
2020-02-16 18:32:47 +0100

    added second server

--- src/api/bot.js
+++ src/api/bot.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+const servers = [
+  "https://pinterest-bot-server1.herokuapp.com/",
+  "https://pinterest-bot-server2.herokuapp.com/"
+];
+
+const randomServer = servers[Math.floor(Math.random() * servers.length)];
+
 export default axios.create(
   {
-    baseURL: "https://pinterest-bot-server1.herokuapp.com/"
+    baseURL: randomServer
   }
-)
+);
