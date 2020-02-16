f2584432860613171c211855063fd079b3989ccc
JrTRD <ayoubtrdbuzzer@gmail.com>
2020-02-16 18:56:16 +0100

    added 5 servers

--- src/api/bot.js
+++ src/api/bot.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 const servers = [
   "https://pinterest-bot-server1.herokuapp.com/",
-  "https://pinterest-bot-server2.herokuapp.com/"
+  "https://pinterest-bot-server2.herokuapp.com/",
+  "https://pinterest-bot-server3.herokuapp.com/",
+  "https://pinterest-bot-server4.herokuapp.com/",
+  "https://pinterest-bot-server5.herokuapp.com/"
 ];
 
 const randomServer = servers[Math.floor(Math.random() * servers.length)];
